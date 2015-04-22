f5b21e4617dabc2899499da3aa8aa93caf4c7b13
pat <patrick.h.needham@gmail.com>
2015-04-22 16:04:00 -0400

    removing bower and font-awesome from github

--- ReadMe.md
+++
@@ -1,2 +0,0 @@
--parallax on the logo always in the background with divs sliding over on top
--jquery side tab to pop out on hover
--- app/scripts/controllers/NavbarController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('MainController').controller('NavbarController', navbarController);
-
-navbarController.$inject = [];
-
-function navbarController(){
-  console.log('hi from the navbar controller');
-}
