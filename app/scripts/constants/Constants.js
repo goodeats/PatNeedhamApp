aaed1ffe3664df368d414b27cfd83be9b8990d37
pat <patrick.h.needham@gmail.com>
2015-05-03 15:52:12 -0400

    Initial dist subtree commit

--- app/scripts/constants/Constants.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-angular.module('PatApp').constant('ServerUrl', 'http://localhost:3000');
--- app/scripts/controllers/NavbarController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function navbarController(){
-  console.log('hi from the navbar controller');
-}
-
-angular.module('MainController').controller('NavbarController', navbarController);
-
-navbarController.$inject = [];
