5158877940b0f76e44e9d6c81b554ffb88025939
pat <patrick.h.needham@gmail.com>
2015-05-08 15:48:48 -0400

    updated Work projects, threw some target='_blank's on the links

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -3,7 +3,7 @@
 function homeController(){
   var vm = this;
 
-  vm.home = 'welcome to my website!';
+  vm.home = 'welcome to my website built with Angular.js!';
 }
 
 angular.module('MainController').controller('HomeController', homeController);
